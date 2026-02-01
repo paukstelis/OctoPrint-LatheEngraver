2264defd5b33d5e914fc58a476df9751e34d5705
paukstelis <paukstel@umd.edu>
2026-02-01 08:36:59 -0500

    update some settings

--- octoprint_latheengraver/__init__.py
+++ octoprint_latheengraver/__init__.py
@@ -2183,11 +2183,6 @@ class LatheEngraverPlugin(octoprint.plugin.SettingsPlugin,
                     ],
                 pip='https://github.com/paukstelis/OctoPrint-LatheEngraver/archive/{target_version}.zip'))
 
-
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-
 __plugin_name__ = 'LatheEngraver Support'
 __plugin_pythoncompat__ = ">=2.7,<4"
 
